39b29aff81bf1757daef941473496c3dc7aa176e
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2021-12-28 09:15:33 +0100

    Improve scripts

--- build/netcore/build-netcore.ps1
+++ build/netcore/build-netcore.ps1
@@ -1,18 +1,36 @@
 <#
   .SYNOPSIS
   This script build and tests a .NET Core application
+  .PARAMETER projectsToBuild
+  The projects to build
+  .PARAMETER projectsToTest
+  The projects to test
+  .PARAMETER verbosity
+  The verbosity level
 #>
+
+[CmdletBinding()]
+Param(
+  [parameter(Mandatory = $true)]
+  [string]$projectsToBuild,
+
+  [parameter(Mandatory = $true)]
+  [string]$projectsToTest,
+
+  [parameter(Mandatory = $true)]
+  [string]$verbosity
+)
 
 Write-Output '=========='
 Write-Output 'Install packages...'
-run: dotnet restore ${{ inputs.projectsToBuild }} --verbosity ${{ inputs.verbosity }}
+run: dotnet restore $projectsToBuild --verbosity $verbosity
  
 Write-Output '=========='
 Write-Output 'Build application...'
-run: dotnet build ${{ inputs.projectsToBuild }} --configuration Release --no-restore --verbosity ${{ inputs.verbosity }}
+run: dotnet build $projectsToBuild --configuration Release --no-restore --verbosity $verbosity
 
 Write-Output '=========='
 Write-Output 'Run tests...'
-run: dotnet test ${{ inputs.projectsToTest }} --configuration Release --no-restore --no-build --verbosity ${{ inputs.verbosity }}
+run: dotnet test $projectsToTest --configuration Release --no-restore --no-build --verbosity $verbosity
 
 Write-Output '=========='
