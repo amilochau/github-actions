778f92bbc1e376638bbfe15bebcad2a25aaec97b
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-01-12 21:45:38 +0100

    Use [System.IO.Compression.ZipFile]::CreateFromDirectory

--- deploy/functions/deploy-functions.ps1
+++ deploy/functions/deploy-functions.ps1
@@ -34,7 +34,8 @@ Write-Output 'Create deployment package...'
 $currentDate = Get-Date -Format yyyyMMdd_HHmmss
 $fileName = "FunctionApp_$currentDate.zip"
 #Compress-Archive -Path ./output/* -DestinationPath $fileName
-zip -r $fileName ./output
+#(Set-Location ./output && zip -r ../$fileName ./output)
+[System.IO.Compression.ZipFile]::CreateFromDirectory('./output', $fileName)
 Write-Output "Deployment package has been created ($fileName)."
 
 Write-Output '=========='
