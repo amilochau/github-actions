d4dc7b6297251b0bc02dda8ca6ce0f591c51769e
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-01-17 23:36:26 +0100

    Use Azure PowerShell in scripts instead of azure cli (#80)
    * Use Azure PowerShell in scripts instead of azure cli
    
    * Add default empty template type
    
    * Force install PowerShell 7.1.0
    
    * Change scope for Azure PowerShell installation
    
    * Enable AzPSSession
    
    * Convert from AzureRM to Az
    
    * Don't install Azure PowerShell anymore
    
    * List Az modules available
    
    * Try using azure/powershell task
    
    * Fix
    
    * Use Az v7.1.0
    
    * Fix extra parameters
    
    * Use azure/powershell for Functions deployment
    
    * Fix Functions deployment working directory
    
    * Fix locations in Functions deployment
    
    * Enable AzPSSession for Functions deployment
    
    * Fixes
    
    * Keep Functions application package
    
    * Fix missing application package URI
    
    * Fix file path for Functions deployment
    
    * Add runtime and self contained properties on Functions publication
    
    * Fix location

--- deploy/functions/deploy-functions.ps1
+++ deploy/functions/deploy-functions.ps1
@@ -1,6 +1,8 @@
 <#
   .SYNOPSIS
   This script deploys a Functions application
+  .PARAMETER projectsToPublishPath
+  The path of the projects to publish
   .PARAMETER verbosity
   The verbosity level
   .PARAMETER applicationName
@@ -12,6 +14,9 @@
 [CmdletBinding()]
 Param(
   [parameter(Mandatory = $true)]
+  [string]$projectsToPublishPath,
+
+  [parameter(Mandatory = $true)]
   [string]$verbosity,
 
   [parameter(Mandatory = $true)]
@@ -21,56 +26,61 @@ Param(
   [string]$healthUrl
 )
 
+Write-Output "Projects to publish path is: $projectsToPublishPath"
 Write-Output "Verbosity is: $verbosity"
 Write-Output "Application name is: $applicationName"
 Write-Output "Health URL is: $healthUrl"
 
 Write-Output '=========='
+Write-Output 'Moving into projects to publish path...'
+Set-Location $projectsToPublishPath
+
+Write-Output '=========='
 Write-Output 'Publish application...'
-dotnet publish --configuration Release --output ./output --verbosity $verbosity
+dotnet publish --configuration Release --runtime linux-x64 --no-self-contained --output ./output --verbosity $verbosity
 
 Write-Output '=========='
 Write-Output 'Create deployment package...'
 $currentDate = Get-Date -Format yyyyMMdd_HHmmss
+$currentLocation = Get-Location
 $fileName = "FunctionsApp_$currentDate.zip"
-[System.IO.Compression.ZipFile]::CreateFromDirectory('./output', $fileName)
-Write-Output "Deployment package has been created ($fileName)."
+$filePath = "$currentLocation/$fileName"
+[System.IO.Compression.ZipFile]::CreateFromDirectory("$currentLocation/output", $filePath)
+Write-Output "Deployment package has been created ($filePath)."
 
 Write-Output '=========='
 Write-Output 'Get application information from application settings...'
-$app = az functionapp list --query "[?name == '$applicationName']" | ConvertFrom-Json
-$resourceGroupName = $app.resourceGroup
-$applicationType = $app.type
-$defaultHostName = $app.defaultHostName
+$app = Get-AzFunctionApp | Where-Object { $_.Name -eq $applicationName } # Would be easier to get with resource group @next-major-version
+$resourceGroupName = $app.ResourceGroupName
+$applicationType = $app.Type
+$defaultHostName = $app.DefaultHostName
 Write-Output "Resource group name: $resourceGroupName"
 Write-Output "Application type: $applicationType"
 Write-Output "Default host name: $defaultHostName"
 
-$appSettings = az functionapp config appsettings list --name $applicationName --resource-group $resourceGroupName | ConvertFrom-Json
-$storageAccountName = $appSettings | Where-Object { $_.name -eq "AzureWebJobsStorage__accountName" } | ForEach-Object { $_.value }
+$appSettings = Get-AzFunctionAppSetting -Name $applicationName -ResourceGroupName $resourceGroupName
+$storageAccountName = $appSettings['AzureWebJobsStorage__accountName']
 Write-Output "Storage account name: $storageAccountName"
 
 Write-Output '=========='
 Write-Output 'Upload package to blob storage...'
 $containerName = 'deployment-packages'
-az storage blob upload `
-  --account-name $storageAccountName `
-  --container-name $containerName `
-  --name $fileName `
-  --file $fileName `
-  --auth-mode login `
-  --no-progress | Out-Null
+
+New-AzStorageContext -StorageAccountName $storageAccountName | Set-AzStorageBlobContent `
+  -Container $containerName `
+  -Blob $fileName `
+  -File $filePath ` | Out-Null
 
 $blobUri = "https://$storageAccountName.blob.core.windows.net/$containerName/$fileName"
 Write-Output "Deployment package has been uploaded to $blobUri."
 
 Write-Output '=========='
 Write-Output 'Update Functions appsettings with package reference...'
-az functionapp config appsettings set --name $applicationName --resource-group $resourceGroupName --settings "WEBSITE_RUN_FROM_PACKAGE=$blobUri" | Out-Null
+Update-AzFunctionAppSetting -Name $applicationName -ResourceGroupName $resourceGroupName -AppSetting @{"WEBSITE_RUN_FROM_PACKAGE" = $blobUri} | Out-Null
 
 Write-Output '=========='
 Write-Output 'Synchronize triggers...'
-az resource invoke-action --resource-group $resourceGroupName --action syncfunctiontriggers --name $applicationName --resource-type $applicationType
+Invoke-AzResourceAction -ResourceGroupName $resourceGroupName -ResourceType $applicationType -ResourceName $applicationName -Action syncfunctiontriggers -Force | Out-Null
 
 Write-Output '=========='
 Write-Output 'Check application health...'
