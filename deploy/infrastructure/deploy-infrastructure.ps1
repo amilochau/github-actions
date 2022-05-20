7590a29f4955180a88741809023370551b5cc7b5
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-05-20 23:34:44 +0200

    Get Key Vault secrets for infrastructure deployment (#94)

--- deploy/infrastructure/deploy-infrastructure.ps1
+++ deploy/infrastructure/deploy-infrastructure.ps1
@@ -170,6 +170,23 @@ if ($scopeType -eq 'resourceGroup') {
   }
 
   Write-Output '=========='
+  Write-Output 'Get Key Vault secrets...'
+  if ($templateType -eq 'functions') {
+    $kv = Get-AzKeyVault -ResourceGroupName $resourceGroupName
+    if (!!$kv) {
+      $secrets = Get-AzKeyVaultSecret $kv.VaultName | ForEach-Object { $_.Name }
+      if (($null -ne $secrets) -and ($secrets.Length -gt 0)) {
+        foreach ($secret in $secrets) {
+          $templateExtraParameters.Add('extraAppSettings', $secret)
+        }
+        Write-Output "Key Vault secrets found ($($secrets.Length) secrets)."
+      }
+    } else {
+      Write-Output "Key Vault not found."
+    }
+  }
+
+  Write-Output '=========='
   Write-Output 'Deploy ARM template file...'
   $result = New-AzResourceGroupDeployment `
     -Name $deploymentName `
