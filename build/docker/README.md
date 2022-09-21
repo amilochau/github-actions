f973e7e2d5459c72c32a4d52d197d8e37886f4b2
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2022-09-21 10:50:06 +0200

    Build and deploy Docker application

---
+++ build/docker/README.md
@@ -0,0 +1,54 @@
+# Readme - build/docker
+
+## Introduction
+
+`amilochau/github-actions/build/docker` is a GitHub Action developed to build a Docker application.
+
+---
+
+## Usage
+
+Use this GitHub action if you want have an application defined with a Dockerfile that you want to build, typically in a Continuous Integration process.
+
+### Example workflow
+
+```yaml
+name: Build
+
+on: workflow_dispatch
+
+jobs:
+  build:
+    name: Build
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - name: Build application
+        uses: amilochau/github-actions/build/docker@v3
+        with:
+          context: ${{ env.DOCKER_IMAGE_CONTEXT }}
+          dockerfile: ${{ env.DOCKER_FILE }}
+          dockerRegistryUsername: ${{ github.actor }}
+          dockerRegistryPassword: ${{ github.token }}
+          dockerImageName: ${{ env.DOCKER_IMAGE_NAME }}
+          dockerImageTag: ${{ github.sha }}
+```
+
+### Inputs
+
+| Input | Description | Required | Default value | Comment |
+| ----- | ----------- | -------- | ------------- | ------- |
+| `context` | The path of the context where to build | **true** |
+| `dockerfile` | The path of the Dockerfile | **true** |
+| `dockerRegistryHost` | The host of the Docker registry | *false* | `ghcr.io/` | Should end with a leading slash |
+| `dockerRegistryUsername` |The username to login to Docker registry | **true** | 
+| `dockerRegistryPassword` | The password to login to Docker registry | **true** | 
+| `dockerImageName` | The name of the Docker image | **true** | 
+| `dockerImageTag` | The tag of the Docker image | **true** | 
+
+### Outputs
+
+| Output | Description |
+| ------ | ----------- |
+
+*No output is defined in the current GitHub Action...*
---
+++ build/docker/build-docker.ps1
@@ -0,0 +1,72 @@
+<#
+  .SYNOPSIS
+  This script builds a Docker application
+  .PARAMETER context
+  The path of the context where to build
+  .PARAMETER dockerfile
+  The path of the Dockerfile
+  .PARAMETER dockerRegistryHost
+  The host of the Docker registry - with a leading slash
+  .PARAMETER dockerRegistryUsername
+  The username to login to Docker registry
+  .PARAMETER dockerRegistryPassword
+  The password to login to Docker registry
+  .PARAMETER dockerImageName
+  The name of the Docker image
+  .PARAMETER dockerImageTag
+  The tag of the Docker image
+#>
+
+[CmdletBinding()]
+Param(
+  [parameter(Mandatory = $true)]
+  [string]$context,
+
+  [parameter(Mandatory = $true)]
+  [string]$dockerfile,
+
+  [parameter(Mandatory = $true)]
+  [string]
+  $dockerRegistryHost,
+
+  [parameter(Mandatory = $true)]
+  [securestring]
+  $dockerRegistryUsername,
+
+  [parameter(Mandatory = $true)]
+  [securestring]
+  $dockerRegistryPassword,
+
+  [parameter(Mandatory = $true)]
+  [string]
+  $dockerImageName,
+
+  [parameter(Mandatory = $true)]
+  [string]
+  $dockerImageTag
+)
+
+Write-Output "Context is: $context"
+Write-Output "Docker file is: $dockerfile"
+Write-Output "Docker registry host is: $dockerRegistryHost"
+Write-Output "Docker image name is: $dockerImageName"
+Write-Output "Docker image tag is: $dockerImageTag"
+
+Write-Output '=========='
+Write-Output 'Set local variables...'
+$dockerImageReference = "$($dockerRegistryHost)$($dockerImageName):$($dockerImageTag)"
+Write-Output "Docker image reference: $dockerImageReference"
+
+Write-Output '=========='
+Write-Output 'Building Docker image...'
+docker build $context --file $dockerfile --tag $dockerImageReference
+
+Write-Output '=========='
+Write-Output 'Loging to Container Registry...'
+docker login $dockerRegistryHost --username $dockerRegistryUsername --password $dockerRegistryPassword
+
+Write-Output '=========='
+Write-Output 'Pushing to Container Registry...'
+docker push $dockerImageReference
+
+Write-Output '=========='
