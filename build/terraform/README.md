afa09515926ffd3a2462c4a3196f7d2558002722
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-02-26 16:51:44 +0100

    Add a workflow to build Terraform modules (#138)

---
+++ build/terraform/README.md
@@ -0,0 +1,43 @@
+# Readme - build/netcore
+
+## Introduction
+
+`amilochau/github-actions/build/terraform` is a GitHub Action developed to build and test Terraform modules.
+
+---
+
+## Usage
+
+Use this GitHub action if you want have one or many Terraform modules that you want to build and test, typically in a Continuous Integration process.
+
+### Example workflow
+
+```yaml
+name: Build
+
+on: workflow_dispatch
+
+jobs:
+  build:
+    name: Build
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - name: Build and test Terraform modules
+        uses: amilochau/github-actions/build/terraform@v3
+```
+
+### Inputs
+
+| Input | Description | Required | Default value | Comment |
+| ----- | ----------- | -------- | ------------- | ------- |
+| `modulesPath` | The path to the Terraform modules to build | *false* | `.tf` |
+| `modulesPathDepth` | The path to the projects to test - can be a .csproj or a .sln file | *false* | `1` |
+| `verbosity` | The verbosity of the scripts | *false* | `minimal` | Set to `minimal`, `normal` or `detailed` |
+
+### Outputs
+
+| Output | Description |
+| ------ | ----------- |
+
+*No output is defined in the current GitHub Action...*
---
+++ build/terraform/build-iac.ps1
@@ -0,0 +1,50 @@
+<#
+  .SYNOPSIS
+  This script builds and tests Terraform modules
+  .PARAMETER modulesPath
+  The path to the Terraform modules to build
+  .PARAMETER modulesPathDepth
+  The depth of the path search, to find the Terraform modules to build
+  .PARAMETER verbosity
+  The verbosity level
+#>
+
+[CmdletBinding()]
+Param(
+  [parameter(Mandatory = $true)]
+  [string]$modulesPath,
+  
+  [parameter(Mandatory = $true)]
+  [int]$modulesPathDepth,
+  
+  [parameter(Mandatory = $true)]
+  [ValidateSet('minimal', 'normal', 'detailed')]
+  [string]$verbosity
+)
+
+Write-Output "Modules path is: $modulesPath"
+Write-Output "Modules path depth is: $modulesPathDepth"
+Write-Output "Verbosity is: $verbosity"
+
+Write-Output '=========='
+
+$pathFilter = 'main.tf'
+$childItems = Get-ChildItem -Path $path -Recurse -Depth $pathDepth -Filter $pathFilter 
+$childItemsCount = $childItems.Length
+Write-Output "Items found: $childItemsCount"
+
+$childItems | Foreach-Object -ThrottleLimit 5 -Parallel {
+  #Action that will run in Parallel. Reference the current object via $PSItem and bring in outside variables with $USING:varname
+  $directoryAbsolutePath = $PSItem.Directory.FullName | Resolve-Path -Relative
+  $directoryRelativePath = $PSItem.Directory.FullName | Resolve-Path -Relative
+  Write-Output "[$directoryRelativePath] Starting..."
+  Set-Location $directoryAbsolutePath
+
+  terraform init -input=false -backend=false
+  terraform validate
+}
+
+Write-Output '=========='
+
+$sw.Stop()
+Write-Output "Job duration: $($sw.Elapsed.ToString("c"))"
