02dffc8c950278965fbc1fa5db094814478f6087
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-03-01 15:43:03 +0100

    Fix 'build/terraform' to use parameters

--- build/terraform/build-iac.ps1
+++ build/terraform/build-iac.ps1
@@ -30,13 +30,13 @@ Write-Output '=========='
 
 $sw = [Diagnostics.Stopwatch]::StartNew()
 $pathFilter = 'main.tf'
-$childItems = Get-ChildItem -Path $path -Recurse -Depth 2 -Filter $pathFilter -Force
+$childItems = Get-ChildItem -Path $modulesPath -Recurse -Depth $modulesPathDepth -Filter $pathFilter -Force
 $childItemsCount = $childItems.Length
 Write-Output "Items found: $childItemsCount"
 
 $childItems | Foreach-Object -ThrottleLimit 5 -Parallel {
   #Action that will run in Parallel. Reference the current object via $PSItem and bring in outside variables with $USING:varname
-  $directoryAbsolutePath = $PSItem.Directory.FullName | Resolve-Path -Relative
+  $directoryAbsolutePath = $PSItem.Directory.FullName
   $directoryRelativePath = $PSItem.Directory.FullName | Resolve-Path -Relative
   Write-Output "[$directoryRelativePath] Starting..."
   Set-Location $directoryAbsolutePath
