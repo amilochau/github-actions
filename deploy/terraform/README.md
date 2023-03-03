7e5fd020dda9b4ce3f13c75e98519c8c929c3219
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-03-03 15:47:07 +0100

    Deploy Terraform module

---
+++ deploy/terraform/README.md
@@ -0,0 +1,43 @@
+# Readme - deploy/terraform
+
+## Introduction
+
+`amilochau/github-actions/deploy/terraform` is a GitHub Action developed to deploy a Terraform module.
+
+---
+
+## Usage
+
+Use this GitHub action if you want have a Terraform module that you want to deploy, typically in a Continuous Deployment process.
+
+### Example workflow
+
+```yaml
+name: Deploy
+
+on: workflow_dispatch
+
+jobs:
+  deploy:
+    name: Deploy
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - name: Deploy a Terraform module
+        uses: amilochau/github-actions/deploy/terraform@v3
+```
+
+### Inputs
+
+| Input | Description | Required | Default value | Comment |
+| ----- | ----------- | -------- | ------------- | ------- |
+| `modulePath` | The path to the Terraform module to deploy | *false* | `.tf` |
+| `workspaceName` | The name of the Terraform workspace | *false* | `default` |
+| `verbosity` | The verbosity of the scripts | *false* | `minimal` | Set to `minimal`, `normal` or `detailed` |
+
+### Outputs
+
+| Output | Description |
+| ------ | ----------- |
+
+*No output is defined in the current GitHub Action...*
---
+++ deploy/terraform/deploy-terraform.ps1
@@ -0,0 +1,61 @@
+<#
+  .SYNOPSIS
+  This script deploy a Terraform module
+  .PARAMETER modulePath
+  The path to the Terraform module to deploy
+  .PARAMETER verbosity
+  The verbosity level
+#>
+
+[CmdletBinding()]
+Param(
+  [parameter(Mandatory = $true)]
+  [string]$modulePath,
+  
+  [parameter(Mandatory = $true)]
+  [ValidateSet('minimal', 'normal', 'detailed')]
+  [string]$verbosity
+)
+
+Write-Output "Modules path is: $modulesPath"
+Write-Output "Modules path depth is: $modulesPathDepth"
+Write-Output "Verbosity is: $verbosity"
+
+Write-Output '=========='
+
+$sw = [Diagnostics.Stopwatch]::StartNew()
+$pathFilter = 'main.tf'
+$childItems = Get-ChildItem -Path $modulesPath -Recurse -Depth $modulesPathDepth -Filter $pathFilter -Force
+$childItemsCount = $childItems.Length
+Write-Output "Items found: $childItemsCount"
+
+$childItems | Foreach-Object -ThrottleLimit 5 -Parallel {
+  #Action that will run in Parallel. Reference the current object via $PSItem and bring in outside variables with $USING:varname
+  $directoryAbsolutePath = $PSItem.Directory.FullName
+  $directoryRelativePath = $PSItem.Directory.FullName | Resolve-Path -Relative
+  Write-Output "[$directoryRelativePath] Starting..."
+  Set-Location $directoryAbsolutePath
+
+  terraform init -input=false -backend=false -upgrade -no-color
+  if (!$?) {
+      Write-Output "::error title=Terraform failed::Terraform initialization failed"
+      throw 1
+  }
+  
+  terraform fmt -check -recursive -no-color
+  if (!$?) {
+      Write-Output "::error title=Terraform failed::Terraform format failed"
+      throw 1
+  }
+
+  terraform validate -json -no-color
+  if (!$?) {
+      Write-Output "::error title=Terraform failed::Terraform validation failed"
+      throw 1
+  }
+}
+
+Write-Output '=========='
+
+$sw.Stop()
+Write-Output "Job duration: $($sw.Elapsed.ToString("c"))"
