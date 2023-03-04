88ecf86f006deeab59791b1d760aa31bf436746e
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-03-04 15:01:10 +0100

    Test with a solution file

--- build/lambda-functions/build-lambda-functions.ps1
+++ build/lambda-functions/build-lambda-functions.ps1
@@ -35,35 +35,43 @@ $dir = (Get-Location).Path
 Write-Output "Pull Docker image, used to build functions"
 docker pull $image -q
 
-$childItems = Get-ChildItem -Path $functionsPath -Recurse -Filter $functionsPathFilter
-$childItemsCount = $childItems.Length
-Write-Output "Items found: $childItemsCount"
-
-$childItems | Foreach-Object -ThrottleLimit 5 -Parallel {
-  $directoryAbsolutePath = $PSItem.Directory.FullName
-  $directoryRelativePath = $PSItem.Directory.FullName | Resolve-Path -Relative
-  $fileRelativePath = $PSItem.FullName | Resolve-Path -Relative
-  Write-Output "[$directoryRelativePath] Starting..."
-  Set-Location $directoryAbsolutePath
-
-  if (Test-Path "$directoryAbsolutePath/obj") {
-    Remove-Item -LiteralPath "$directoryAbsolutePath/obj" -Force -Recurse
-  }
-  if (Test-Path "$directoryAbsolutePath/bin") {
-    Remove-Item -LiteralPath "$directoryAbsolutePath/bin" -Force -Recurse
-  }
-  if (Test-Path "$directoryAbsolutePath/dist") {
-    Remove-Item -LiteralPath "$directoryAbsolutePath/dist" -Force -Recurse
-  }
-
-  docker run --rm -v "$($using:dir):/src" -w /src $using:image dotnet publish "$fileRelativePath" -c Release -f net7.0 -r windows-x64 --sc true -p:BuildSource=AwsCmd /p:GenerateRuntimeConfigurationFiles=true /p:StripSymbols=true
-  if (!$?) {
-    Write-Output "::error title=Build failed::Build failed"
-    throw 1
-  }
-  
-  Write-Output "[$directoryRelativePath] Done."
+$solutionPath = './src/maps-api/Milochau.Maps.sln'
+docker run --rm -v "$($dir):/src" -w /src $image dotnet publish "$solutionPath" -c Release -f net7.0 -r linux-x64 --sc true -p:BuildSource=AwsCmd /p:GenerateRuntimeConfigurationFiles=true /p:StripSymbols=true
+if (!$?) {
+  Write-Output "::error title=Build failed::Build failed"
+  throw 1
 }
+
+
+#$childItems = Get-ChildItem -Path $functionsPath -Recurse -Filter $functionsPathFilter
+#$childItemsCount = $childItems.Length
+#Write-Output "Items found: $childItemsCount"
+#
+#$childItems | Foreach-Object -ThrottleLimit 5 -Parallel {
+#  $directoryAbsolutePath = $PSItem.Directory.FullName
+#  $directoryRelativePath = $PSItem.Directory.FullName | Resolve-Path -Relative
+#  $fileRelativePath = $PSItem.FullName | Resolve-Path -Relative
+#  Write-Output "[$directoryRelativePath] Starting..."
+#  Set-Location $directoryAbsolutePath
+#
+#  if (Test-Path "$directoryAbsolutePath/obj") {
+#    Remove-Item -LiteralPath "$directoryAbsolutePath/obj" -Force -Recurse
+#  }
+#  if (Test-Path "$directoryAbsolutePath/bin") {
+#    Remove-Item -LiteralPath "$directoryAbsolutePath/bin" -Force -Recurse
+#  }
+#  if (Test-Path "$directoryAbsolutePath/dist") {
+#    Remove-Item -LiteralPath "$directoryAbsolutePath/dist" -Force -Recurse
+#  }
+#
+#  docker run --rm -v "$($using:dir):/src" -w /src $using:image dotnet publish "$fileRelativePath" -c Release -f net7.0 -r linux-x64 --sc true -p:BuildSource=AwsCmd /p:GenerateRuntimeConfigurationFiles=true /p:StripSymbols=true
+#  if (!$?) {
+#    Write-Output "::error title=Build failed::Build failed"
+#    throw 1
+#  }
+#  
+#  Write-Output "[$directoryRelativePath] Done."
+#}
 
 Write-Output '=========='
 
