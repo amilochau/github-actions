497a0cc169cce0d991f8876b090add0ee085cbd0
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-03-06 20:31:09 +0100

    Add more logs

--- clean/artifacts/clean-artifacts.ps1
+++ clean/artifacts/clean-artifacts.ps1
@@ -13,14 +13,17 @@ Param(
 )
 
 Write-Output "Verbosity is: $verbosity"
+Write-Output "GitHub environment is: $env:GITHUB_REPOSITORY"
+Write-Output "GitHub run id is: $env:GITHUB_RUN_ID"
+Write-Output "Verbosity is: $verbosity"
 
 Write-Output '=========='
 
 Write-Output 'Getting artifacts from current run...'
+
 $artifactsResponse = gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/$env:GITHUB_REPOSITORY/actions/runs/$env:GITHUB_RUN_ID/artifacts
 
 $artifacts = ($artifactsResponse | ConvertFrom-Json).artifacts
-Write-Output 'Getting artifacts from current run...'
 $artifactsCount = $artifacts.Count
 Write-Output "Items found: $artifactsCount"
 
