c4514f28f30b306d231721cda1b8e1bd7d264642
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-03-06 15:24:28 +0100

    Create artifact when building Lambda Functions

--- build/lambda-functions/build-lambda-functions.ps1
+++ build/lambda-functions/build-lambda-functions.ps1
@@ -3,6 +3,8 @@
   This script builds and tests Terraform modules
   .PARAMETER solutionPath
   The path to the solution file, with functions to deploy
+  .PARAMETER publishPathFilter
+  The path of the files to publish, as a filter to be tested to determine the files to add in the artifact
   .PARAMETER verbosity
   The verbosity level
 #>
@@ -11,6 +13,9 @@
 Param(
   [parameter(Mandatory = $true)]
   [string]$solutionPath,
+
+  [parameter(Mandatory = $true)]
+  [string]$publishPathFilter, # Typically '*/bin/Release/net7.0/linux-x64/publish/bootstrap'
   
   [parameter(Mandatory = $true)]
   [ValidateSet('minimal', 'normal', 'detailed')]
@@ -18,6 +23,7 @@ Param(
 )
 
 Write-Output "Solution path is: $solutionPath"
+Write-Output "Publish path filter is: $publishPathFilter"
 Write-Output "Verbosity is: $verbosity"
 
 Write-Output '=========='
@@ -35,6 +41,57 @@ if (!$?) {
   throw 1
 }
 
+Write-Output '=========='
+
+Write-Output "Finding the files to publish in the artifact..."
+
+$publishPathFilterLinux = $publishPathFilter.Replace("\", "/")
+$publishPathFilterWindows = $publishPathFilter.Replace("/", "\")
+$solutionDirectoryPath = (Get-ChildItem $solutionPath).Directory.FullName
+$childItems = Get-ChildItem -Path $solutionDirectoryPath -Recurse -Include 'bootstrap'
+
+if (Test-Path "./output") {
+  Remove-Item -LiteralPath "./output" -Force -Recurse
+}
+if (Test-Path "./output-compressed") {
+  Remove-Item -LiteralPath "./output-compressed" -Force -Recurse
+}
+New-Item -Path "./output" -ItemType Directory | Out-Null
+New-Item -Path "./output-compressed" -ItemType Directory | Out-Null
+
+Write-Output '=========='
+
+$childItemsCount = $childItems.Count
+Write-Output "Items found: $childItemsCount"
+
+foreach ($childItem in $childItems) {
+  $directoryRelativePath = $childItem.Directory.FullName | Resolve-Path -Relative
+  $fileRelativePath = $childItem.FullName | Resolve-Path -Relative
+
+  if ($fileRelativePath -inotlike $publishPathFilterLinux -and $fileRelativePath -inotlike $publishPathFilterWindows) {
+    Write-Output "[$fileRelativePath] Not treated."
+    continue
+  }
+
+  Write-Output "[$fileRelativePath] Moving file to output..."
+  $directoryDestinationPath = Join-Path "$PWD/output" "$directoryRelativePath"
+  $destinationPath = Join-Path $directoryDestinationPath $childItem.Name
+  if (-not (Test-Path $directoryDestinationPath)) {
+    New-Item -Path $directoryDestinationPath -ItemType Directory | Out-Null
+  }
+  Move-Item -Path $childItem -Destination $destinationPath
+  Write-Output "[$fileRelativePath] File moved to output."
+  
+  Write-Output "-----"
+}
+
+Write-Output '=========='
+
+if ($childItemsCount -gt 0) {
+  Write-Output "Creating compressed file..."
+  $compressedFilePath = './output-compressed/functions.zip'
+  [System.IO.Compression.ZipFile]::CreateFromDirectory("./output", $compressedFilePath)
+}
 Write-Output '=========='
 
 $sw.Stop()
