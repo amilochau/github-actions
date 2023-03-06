554759173c5509f2b38beca86adbf1d8b6d78eda
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-03-06 15:50:04 +0100

    Fix

--- build/lambda-functions/build-lambda-functions.ps1
+++ build/lambda-functions/build-lambda-functions.ps1
@@ -83,8 +83,10 @@ foreach ($childItem in $childItems) {
   Write-Output "[$fileRelativePath] File copied to output."
 
   Write-Output "[$fileRelativePath] Creating compressed file..."
+  $tmpCompressedFilePath = "$directoryDestinationPath.zip"
   $compressedFilePath = "$destinationPath.zip"
-  [System.IO.Compression.ZipFile]::CreateFromDirectory($directoryDestinationPath, $compressedFilePath)
+  [System.IO.Compression.ZipFile]::CreateFromDirectory($directoryDestinationPath, $tmpCompressedFilePath)
+  Move-Item -Path $tmpCompressedFilePath -Destination $compressedFilePath
   Write-Output "[$fileRelativePath] Compressed file created."
 
   Write-Output "-----"
