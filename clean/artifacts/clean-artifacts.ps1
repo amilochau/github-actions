5ca6bc941a870f52f7761e6cd492fa506653301b
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-03-07 18:38:42 +0100

    Add a new Action to clean GitHub Actions artifacts (#150)
    * Add a new Action to clean GitHub Actions artifacts
    
    * Fix multi-line commands
    
    * Set GitHub token
    
    * Add more logs
    
    * Use an explicit input for GitHub token
    
    * Test
    
    * Test
    
    * Fix test
    
    * Fix
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Define explicit runId from inputs
    
    * Fix
    
    * Remove artifacts
    
    * Last improvement

---
+++ clean/artifacts/clean-artifacts.ps1
@@ -0,0 +1,49 @@
+<#
+  .SYNOPSIS
+  This script clean GitHub Actions artifacts
+  .PARAMETER runId
+  The id of the GitHub Actions run from which to clean artifacts
+  .PARAMETER verbosity
+  The verbosity level
+#>
+
+[CmdletBinding()]
+Param(
+  [parameter(Mandatory = $true)]
+  [string]$runId,
+
+  [parameter(Mandatory = $true)]
+  [ValidateSet('minimal', 'normal', 'detailed')]
+  [string]$verbosity
+)
+
+Write-Output "Run Id is: $runId"
+Write-Output "Verbosity is: $verbosity"
+Write-Output "GitHub environment is: $env:GITHUB_REPOSITORY"
+
+Write-Output '=========='
+
+Write-Output 'Getting artifacts from current run...'
+$url = "/repos/$env:GITHUB_REPOSITORY/actions/runs/$runId/artifacts"
+Write-Output "Url is: $url"
+
+$artifactsResponse = gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" $url
+
+$artifacts = ($artifactsResponse | ConvertFrom-Json).artifacts
+$artifactsCount = $artifacts.Count
+Write-Output "Items found: $artifactsCount"
+
+foreach ($artifact in $artifacts) {
+  $artifactName = $artifact.name
+  Write-Output "[$artifactName] Removing artifact..."
+
+  $artifactId = $artifact.id
+  $url = "/repos/$env:GITHUB_REPOSITORY/actions/artifacts/$artifactId"
+  Write-Output "[$artifactName] Url is: $url"
+
+  gh api --method DELETE -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" $url | Out-Null
+
+  Write-Output "[$artifactName] Artifact removed."
+}
+
+Write-Output '=========='
