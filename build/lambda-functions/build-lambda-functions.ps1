e1cebe1463169f29711fce30704c0bbc43295a01
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-03-08 14:47:39 +0100

    Test witout docker

--- build/lambda-functions/build-lambda-functions.ps1
+++ build/lambda-functions/build-lambda-functions.ps1
@@ -29,13 +29,14 @@ Write-Output "Verbosity is: $verbosity"
 Write-Output '=========='
 
 $sw = [Diagnostics.Stopwatch]::StartNew()
-$image = "public.ecr.aws/sam/build-dotnet7:latest-x86_64"
-$dir = (Get-Location).Path
+#$image = "public.ecr.aws/sam/build-dotnet7:latest-x86_64"
+#$dir = (Get-Location).Path
 
 Write-Output "Pull Docker image, used to build functions"
-docker pull $image -q
+#docker pull $image -q
 
-docker run --rm -v "$($dir):/src" -w /src $image dotnet publish "$solutionPath" -c Release -f net7.0 -r linux-x64 --sc true -p:BuildSource=AwsCmd /p:GenerateRuntimeConfigurationFiles=true /p:StripSymbols=true
+#docker run --rm -v "$($dir):/src" -w /src $image dotnet publish "$solutionPath" -c Release -r linux-x64 --sc true -p:BuildSource=AwsCmd
+dotnet publish "$solutionPath" -c Release -r linux-x64 --sc true -p:BuildSource=AwsCmd
 if (!$?) {
   Write-Output "::error title=Build failed::Build failed"
   throw 1
