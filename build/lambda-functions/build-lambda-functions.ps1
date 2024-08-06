38353e8db6ca70de10c06d50523ce28930e80ea5
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2024-08-06 12:51:35 +0200

    Test

--- build/lambda-functions/build-lambda-functions.ps1
+++ build/lambda-functions/build-lambda-functions.ps1
@@ -44,7 +44,7 @@ docker pull $image -q
 $commandAddSource = "dotnet nuget add source --username $repositoryOwner --password $githubToken --store-password-in-clear-text --name github https://nuget.pkg.github.com/$repositoryOwner/index.json"
 $commandPublish = "dotnet publish $solutionPath -c Release -r linux-x64 --sc true -p:BuildSource=AwsCmd"
 
-docker run --rm -v "$($dir):/src" -w /src $image "$commandAddSource && $commandPublish"
+docker run --rm -v "$($dir):/src" -w /src $image $commandAddSource && $commandPublish
 if (!$?) {
   Write-Output "::error title=Build failed::Build failed"
   throw 1
